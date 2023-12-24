4c0ffc24c76ab2e424089342d755fa6172894a9f
rafidulonnoy <rafidulonnoy@gmail.com>
2023-12-25 00:53:43 +0600

    Onek din por

--- test.js
+++ test.js
@@ -1,56 +1,9 @@
-let count = 0;
-function cc(card) {
+function checkObj(obj, checkProp) {
   // Only change code below this line
-  switch (card) {
-    case 2:
-      ++count;
-      break;
-    case 3:
-      ++count;
-      break;
-    case 4:
-      ++count;
-      break;
-    case 5:
-      ++count;
-      break;
-    case 6:
-      ++count;
-      break;
-    case 7:
-      count + 0;
-      break;
-    case 8:
-      count + 0;
-      break;
-    case 9:
-      count + 0;
-      break;
-    case "J":
-      --count;
-      break;
-    case "Q":
-      --count;
-      break;
-    case "K":
-      --count;
-      break;
-    case "A":
-      --count;
-      break;
-  }
-  if (count > 0) {
-    return `${count} Bet`;
-  } else if (count < 0 || count === 0) {
-    return `${count} Hold`;
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
   } else {
-    return "Change Me";
+    return "Not Found";
   }
   // Only change code above this line
 }
-console.log(count);
-cc(2);
-cc(3);
-cc(7);
-cc("K");
-cc("A");
